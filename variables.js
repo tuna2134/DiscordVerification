517186550b6e840575630db8bb3a9e77966d4127
tnt <45692264+tuna2134@users.noreply.github.com>
2020-12-28 22:32:34 +0900

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,10 +1,10 @@
 // Discord info
 const discord_bot_token = '';
-const discord_guild_id = '';
-const discord_verified_role = '';
+const discord_guild_id = '761138135324295169';
+const discord_verified_role = '761138868506329088';
 
 // reCAPTCHA v2 secret key
-const recaptcha_secret_key = '';
+const recaptcha_secret_key = '6LeC4hcaAAAAAFfgZQDba0RSANFO1riTjjm9at9U';
 
 // Domain
 const domain = ''; // Leave empty for localhost
